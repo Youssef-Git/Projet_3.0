76e81508e264d3ff070b2b0ee09c5bc8c235f821
Youssef <youssef91@hotmail.fr>
2023-07-21 10:29:12 +0200

    Connexion a l'api

--- CarApp/App.js
+++ CarApp/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Api from './components/Api';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Api />
       <StatusBar style="auto" />
     </View>
   );
